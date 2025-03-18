6da6e21150cc3dcb634e697487a7f735ee542536
lhoden <laurenoden97@gmail.com>
2025-03-18 00:16:07 -0400

    Added proxy config, bare minimum UI, and a couple of libraries

--- color-swatch-generator/ui/color-swatch-ui/src/components/palette/Palette.tsx
+++ color-swatch-generator/ui/color-swatch-ui/src/components/palette/Palette.tsx
@@ -1,17 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './Palette.css';
+import nearestColor from 'nearest-color';
+import { colornames } from 'color-name-list';
+import {Tooltip} from '@mui/material';
 
 function Palette() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // nearestColor need objects {name => hex} as input
+  const colors = colornames.reduce((o: any, a: any) => Object.assign(o, { [a.name]: a.hex }), {});
+
+  const nearest = nearestColor.from(colors);
+
+  useEffect(() => {
+    fetch("/ask-ai/prompt", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ "prompt": "Can you output a color palette with seven hex codes that would visually describe the following scene: 'It is a beautiful fall October day.'"}),
+    })
+    .then(async (r) => {
+        setLoading(false);
+        if (r.ok) {
+            let data = await r.json();
+            setData(data);
+            console.log('yay: ', data);
+        }
+    });
+  }, []);
+
   return (
     <>
         <div className="container">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+          {(data.map((swatch: any) => (
+            <div className="palette">
+                <div className="color-swatch-name">{swatch.hexCode}</div>
+                <Tooltip title={swatch.description}>
+                  <div className="color-swatch" style={{background: swatch.hexCode}}>
+                    {nearest(swatch.hexCode).name}
+                  </div>
+                </Tooltip>
+            </div>
+          )))}
         </div>
     </>
   )
--- color-swatch-generator/ui/color-swatch-ui/vite.config.ts
+++ color-swatch-generator/ui/color-swatch-ui/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/ask-ai': {
+        target: 'http://localhost:5000', // This will be the aws location of server and exposed port later
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ask-ai/, ''),
+      },
+    },
+  }
 })
