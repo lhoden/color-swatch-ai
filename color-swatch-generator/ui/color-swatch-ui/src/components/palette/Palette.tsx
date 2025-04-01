98e32c5153692c5a56a1e1f3ba613deb6bc4897e
Lauren Oden <laurenoden97@gmail.com>
2025-04-01 12:45:49 -0400

    Added dialogue functionality

--- color-swatch-generator/ui/color-swatch-ui/src/components/palette/Palette.tsx
+++ color-swatch-generator/ui/color-swatch-ui/src/components/palette/Palette.tsx
@@ -2,11 +2,38 @@ import { useState, useEffect } from 'react'
 import './Palette.css';
 import nearestColor from 'nearest-color';
 import { colornames } from 'color-name-list';
-import {Tooltip, Typography} from '@mui/material';
+import './Animation.scss';
+import { Box, Button, Modal, Typography, Tooltip } from '@mui/material';
+import Textarea from '@mui/joy/Textarea';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  color: '#000',
+  p: 4,
+};
+
 
 function Palette() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [prompt, setPrompt] = useState('');
+
+  const handlePromptChange = (event: any) => {
+    setPrompt(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    promptAI();
+    setOpen(false);
+  }
 
   // nearestColor need objects {name => hex} as input
   const colors = colornames.reduce((o: any, a: any) => Object.assign(o, { [a.name]: a.hex }), {});
@@ -14,12 +41,16 @@ function Palette() {
   const nearest = nearestColor.from(colors);
 
   useEffect(() => {
+    promptAI();
+  }, []);
+
+  const promptAI = () => {
     fetch("/ask-ai/prompt", {
       method: "POST",
       headers: {
           "Content-Type": "application/json",
       },
-      body: JSON.stringify({ "prompt": "Can you output a color palette with seven hex codes that would visually describe the following scene: 'It is a beautiful fall October day.'"}),
+      body: JSON.stringify({ "prompt": `Can you output a color palette with seven hex codes that would visually describe the following scene: '${prompt ? prompt : 'It is a beautiful fall October day.'}'`}),
     })
     .then(async (r) => {
         setLoading(false);
@@ -29,10 +60,27 @@ function Palette() {
             console.log('yay: ', data);
         }
     });
-  }, []);
+  }
 
   return (
     <>
+      <div style={{height: '5em'}}>
+        <button className="raise" onClick={() => {setOpen(true)}}>Generate</button>
+      </div>
+      <Modal
+          open={open}
+          onClose={() => {setOpen(false)}}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Describe your visual
+            </Typography>
+            <Textarea minRows={2} variant="outlined" value={prompt} onChange={handlePromptChange}/>
+            <Button onClick={handleSubmit}>Go</Button>
+          </Box>
+        </Modal>
         <div className="container">
           {(data.map((swatch: any) => (
             <div className="palette">
